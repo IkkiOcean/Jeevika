45ad2f7133a304f64a141e3d9e4cf3367b473e1d
Ikki <vivekprakash.st@gmail.com>
2024-09-19 00:33:10 +0530

    minor-css-fix

--- Vending/server/jeevika/dispense.py
+++ Vending/server/jeevika/dispense.py
@@ -7,29 +7,30 @@ input_pin = [27,22,16,5]
 
 
 def dispense_med(medicine_id, address, quantity):
-        GPIO.setmode(GPIO.BCM)
+        # GPIO.setmode(GPIO.BCM)
         
 
         for i in range(quantity):
             try:
                 pin = raspberry_map[address-1][1]
                 print(pin)
-                GPIO.setup(pin,GPIO.OUT)
-                GPIO.output(pin, GPIO.HIGH)
-                start = time.time()
-                time.sleep(0.05)
-                GPIO.output(pin,GPIO.LOW)
-                while True:
-                    curr = time.time()
-                    if(GPIO.input(input_pin[address-1]) == GPIO.HIGH):
-                        print("dispensed")
-                        break
-                    if (start - time.time() > 3):
-                        raise e
-                        break
+                # GPIO.setup(pin,GPIO.OUT)
+                # GPIO.setup(input_pin[address-1],GPIO.IN)
+                # GPIO.output(pin, GPIO.HIGH)
+                # start = time.time()
+                # time.sleep(0.05)
+                # GPIO.output(pin,GPIO.LOW)
+                # while True:
+                #     curr = time.time()
+                    # if(GPIO.input(input_pin[address-1]) == GPIO.HIGH):
+                    #     print("dispensed")
+                    #     break
+                    # if (start - time.time() > 3):
+                    #     raise e
+                        # break
 
                 print("dispensed successfully!")
-                GPIO.cleanup()
+                # GPIO.cleanup()
                 time.sleep(0.5)
                 
             except Exception as e:
